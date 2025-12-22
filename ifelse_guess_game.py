a82c8e070f0355a7e24a900044122b5c103a3280
Thats private <divesh.jadhwani.vf@sse.ac.in>
2025-12-22 09:08:15 +0530

    Update ifelse_guess_game.py

--- ifelse_guess_game.py
+++ ifelse_guess_game.py
@@ -2,13 +2,13 @@ import streamlit as st
 import random
 
 st.title("🎮 Guess The Number Game")
-st.write("Guess a number between 1 and 10. You get 6 chances!")
+st.write("Guess a number between 1 and 50. You get 6 chances!")
 
 # -------------------------
 # SESSION STATE
 # -------------------------
 if "secret" not in st.session_state:
-    st.session_state.secret = random.randint(1, 10)
+    st.session_state.secret = random.randint(1, 50)
 
 if "attempts" not in st.session_state:
     st.session_state.attempts = 6
@@ -44,6 +44,7 @@ if st.button("Submit Guess") and not st.session_state.game_over:
     # If attempts finished → GAME OVER
     if st.session_state.attempts == 0 and not st.session_state.game_over:
         st.error(f"❌ Game Over! You used all attempts. Number was {st.session_state.secret}")
+        st.write(f"The number was: {st.session_state.secret}")
         st.session_state.game_over = True
 
 
